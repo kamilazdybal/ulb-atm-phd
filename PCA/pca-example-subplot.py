06f3f3a9283395eea6dc2b1925df5e0e51b87725
camillejr <kamilazdybal@gmail.com>
2019-02-04 11:52:39 +0100

    Plot update: increase font

--- PCA/pca-example-subplot.py
+++ PCA/pca-example-subplot.py
@@ -12,6 +12,7 @@ ln = 1
 # Fonts:
 csfont = {'fontname':'cmr10'}
 hfont = {'fontname':'cmr10'}
+font_title = 18
 
 # Generate randomized dataset with linearity
 Np = 100
@@ -35,13 +36,13 @@ Dataset_approx = np.dot(pca.transform(Dataset)[:,:q], pca.components_[:q,:]) + n
 figure = plt.figure(figsize=(11, 8))
 plt.subplot(2,3,1)
 plt.scatter(Dataset[:,0], Dataset[:,1], color=lineColour, marker='.', linewidth=ln)
-plt.title('Raw dataset', **csfont)
+plt.title('Raw dataset', **csfont, fontsize=font_title)
 plt.axis('equal')
 plt.yticks([3, 4, 5, 6]), plt.xticks([3, 4, 5, 6])
 
 plt.subplot(2,3,2)
 plt.scatter(Dataset_proc[:,0], Dataset_proc[:,1], color=lineColour, marker='.', linewidth=ln)
-plt.title('Dataset centered', **csfont)
+plt.title('Dataset centered', **csfont, fontsize=font_title)
 plt.axis('equal')
 plt.yticks([-1, 0, 1]), plt.xticks([-1, 0, 1])
 
@@ -51,25 +52,25 @@ plt.scatter(Dataset_proc[:,0], Dataset_proc[:,1], color=lineColour, marker='.', 
 # since the larger it is, the shorter the vector.
 plt.quiver(PCs[0,0], PCs[0,1], scale=70*(1-eigvals[0]), color=PCColour, width=0.01)
 plt.quiver(PCs[1,0], PCs[1,1], scale=10*(1-eigvals[1]), color=PCColour, width=0.01)
-plt.title('Principal components', **csfont)
+plt.title('Principal components', **csfont, fontsize=font_title)
 plt.axis('equal')
 plt.yticks([-1, 0, 1]), plt.xticks([-1, 0, 1])
 
 plt.subplot(2,3,4)
 plt.scatter(scores[:,0], scores[:,1], color=scoresColour, marker='.', linewidth=ln)
-plt.title('PC-scores', **csfont)
+plt.title('Data rotation', **csfont, fontsize=font_title)
 plt.axis('equal')
 plt.yticks([-2, 0, 2]), plt.xticks([-2, 0, 2])
 
 plt.subplot(2,3,5)
 plt.scatter([Dataset_projected], [np.mat(np.zeros(Np))], color=scoresColour, marker='.', linewidth=ln)
-plt.title('Data projection', **csfont)
+plt.title('Data projection', **csfont, fontsize=font_title)
 plt.axis('equal')
 plt.yticks([0]), plt.xticks([-2, 0, 2])
 
 plt.subplot(2,3,6)
 plt.scatter([Dataset_approx[:,0]], [Dataset_approx[:,1]], color=lineColour, marker='.', linewidth=ln)
-plt.title('Data approximation', **csfont)
+plt.title('Data approximation', **csfont, fontsize=font_title)
 plt.axis('equal')
 plt.yticks([3, 4, 5, 6]), plt.xticks([3, 4, 5, 6])
 
