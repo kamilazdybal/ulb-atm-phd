8035b77520b2f9e5b197dd856d33caf76da9814b
camillejr <kamilazdybal@gmail.com>
2019-04-02 16:05:34 +0200

    GPR updates

--- GPR/GPR-Python/GPR-george.py
+++ GPR/GPR-Python/GPR-george.py
@@ -14,9 +14,16 @@ font_axis = 12
 
 ## Code
 ## -------
+# Training input:
 x = np.array([-1.5, -1.2, -0.5, 0.5, 1.5])
+
+# Training output:
+y = np.sin(2*x)
+
+# 100 points for which we would like the GPR prediction:
+x_pred = np.linspace(-2, 2, 100)
+
 yerr = np.zeros_like(x)
-y = np.sin(2*x) + 1/(2*math.pi*0.5)**0.5 * math.e**((-(x + 1)**2)/(2*0.5**2))*5
 
 # Plot data points:
 figure = plt.figure(figsize=(11, 7))
@@ -28,16 +35,15 @@ kernel = np.var(y) * george.kernels.ExpSquaredKernel(0.1)
 gp = george.GP(kernel)
 gp.compute(x, yerr)
 
-x_pred = np.linspace(-2, 2, 100)
 pred, pred_var = gp.predict(y, x_pred, return_var=True)
 plt.fill_between(x_pred, pred - np.sqrt(pred_var), pred + np.sqrt(pred_var), color="k", alpha=0.2)
 plt.plot(x_pred, pred, "k", lw=1.5, alpha=0.5)
 plt.errorbar(x, y, yerr=yerr, fmt=".k", capsize=0)
 
 # Real data model:
-plt.plot(x_pred, np.sin(2*x_pred) + 1/(2*math.pi*0.5)**0.5 * math.e**((-(x_pred + 1)**2)/(2*0.5**2))*5, "--r", lineWidth=3)
+plt.plot(x_pred, np.sin(2*x_pred), "--r", lineWidth=3)
 plt.xlim(-2, 2)
-plt.ylim(-1.2, 2.2)
+plt.ylim(-1.5, 1.5)
 plt.xlabel(r'x', fontsize=font_label, **csfont)
 plt.ylabel(r'y', fontsize=font_label, **csfont)
 
